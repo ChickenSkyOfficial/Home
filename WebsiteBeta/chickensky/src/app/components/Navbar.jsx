62a325fa0ee0ebdb94c41af8719086f561c72cb7
BrainGHG <120205633+jobrain1@users.noreply.github.com>
2024-09-04 16:25:13 +0200

    modified:   WebsiteBeta/chickensky/src/app/components/Navbar.jsx

--- WebsiteBeta/chickensky/src/app/components/Navbar.jsx
+++ WebsiteBeta/chickensky/src/app/components/Navbar.jsx
@@ -28,9 +28,9 @@ const Navbar = () => {
       <div className="container flex flex-wrap items-center justify-between px-4 py-2 mx-auto lg:py-4">
         <Link
           href={"/"}
-          className="text-2xl font-semibold text-white md:text-5xl"
+          className="text-2xl font-semibold text-white md:text-3xl"
         >
-          ChickenSky
+          ChickenSky.de
         </Link>
         <div className="block mobile-menu md:hidden">
           {!navbarOpen ? (
